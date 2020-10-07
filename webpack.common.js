057f6229a945fc130bd08e13ade7c6c1ef1145e6
Felix Anducho <felix.anducho74@gmail.com>
2020-10-07 18:17:40 -0500

    Splitting dev and prod configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "build"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
