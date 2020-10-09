c819dd6449f5f82beefdc2a13afb6cdb8cf4d628
Felix Anducho <felix.anducho74@gmail.com>
2020-10-09 18:19:20 -0500

    Extract the CSS to a file instead of being injected to the dom in production. Minify HTML/CSS/JS

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   entry: "./src/index.js",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
       {
         test: /\.html$/i,
         use: "html-loader",
