1d7093df882d9ed879a7dac207acc6d5846bc9c2
Felix Anducho <felix.anducho74@gmail.com>
2020-10-23 17:31:52 -0500

    Added minor styles to modal. Fix alert, now it shows outside of the main container

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ import { Notification } from "./Notification";
 document.addEventListener("DOMContentLoaded", function () {
   const releasesButton = document.querySelector(".input-container__getcontent");
   const inputContainer = document.querySelector(".input-container");
+  const releasesContainer = document.querySelector(".releases");
 
   releasesButton.addEventListener("click", async function () {
-    if (inputContainer.children.length === 4) {
-      inputContainer.lastElementChild.remove();
+    const alertRemove = releasesContainer.previousSibling;
+
+    if (alertRemove.className === "alert") {
+      inputContainer.nextElementSibling.remove();
     }
 
     let releases = await GetReleases();
 
     if (releases.message === "Artist not found.") {
       const alert = Notification("Please, try again!", "alert");
-      return inputContainer.appendChild(alert);
+      return inputContainer.parentNode.insertBefore(alert, releasesContainer);
     }
 
     ShowReleases(releases);
